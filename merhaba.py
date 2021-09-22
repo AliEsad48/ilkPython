0233349c57842d0e4d6d4bdb4e704a9d4e4ecf82
Salih Bayram <alicamci2020@gmail.com>
2021-09-22 11:51:52 +0300

    V1

---
+++ merhaba.py
@@ -0,0 +1,8 @@
+print("Starbucksa Hoşgeldiniz")
+kahve=int(input("Lütfen almak istediğiniz içeceği seçiniz\n1-Mocca\n2-Cappicino\n3-Fropocino\n"))
+if kahve==1:
+    print("Mocca aldınız ödemeniz gereken tutar : 10₺")
+elif kahve==2:
+    print("Cappicino aldınız ödemeniz gereken tutar : 18₺")
+elif kahve==3:
+    print("Fropocino aldınız ödemeniz gereken tutar : 9₺")
