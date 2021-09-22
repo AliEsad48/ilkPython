0ecafabafe188bb871eeba07f521349e435baa9a
Salih Bayram <alicamci2020@gmail.com>
2021-09-22 20:10:18 +0300

    V2

--- merhaba.py
+++ merhaba.py
@@ -1,8 +1,21 @@
-print("Starbucksa Hoşgeldiniz")
-kahve=int(input("Lütfen almak istediğiniz içeceği seçiniz\n1-Mocca\n2-Cappicino\n3-Fropocino\n"))
-if kahve==1:
-    print("Mocca aldınız ödemeniz gereken tutar : 10₺")
-elif kahve==2:
-    print("Cappicino aldınız ödemeniz gereken tutar : 18₺")
-elif kahve==3:
-    print("Fropocino aldınız ödemeniz gereken tutar : 9₺")
+bakiye=1000
+def paraYatirmak(bakiye):
+    ypara=int(input("Lütfen yatırmak istediğiniz miktarı giriniz :"))
+    bakiye+=ypara
+    print("Bakiyeniz : " , bakiye)
+def paraCekmek(bakiye):
+    cpara=int(input("Lütfen çekmek istediğiniz miktarı giriniz : "))
+    if cpara<=bakiye:
+        print("Kalan Bakiyeniz:" , bakiye)
+    else:
+        print("Bakiyenizden fazla para çekemezsiniz")
+def krediCekmek():
+    a=int(input("Lütfen çekmek istediğiniz miktarı giriniz :"))
+    print("Başarıyla kredi çektiniz")
+islem=int(input("Lütfen yapmak istediğiniz işlemi seçiniz \n1-Para Yatırmak\n2-Para Çekmek\n3-Kredi Çekmek\n"))
+if islem==1:
+    paraYatirmak(bakiye)
+elif islem==2:
+    paraCekmek(bakiye)
+elif islem==3:
+    krediCekmek()
