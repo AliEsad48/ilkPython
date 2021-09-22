cfd4575a4631aa63dbb421c58466e76ee82c6485
Salih Bayram <alicamci2020@gmail.com>
2021-09-22 21:01:53 +0300

    V3

--- merhaba.py
+++ merhaba.py
@@ -1,4 +1,5 @@
 bakiye=1000
+islem=None
 def paraYatirmak(bakiye):
     ypara=int(input("Lütfen yatırmak istediğiniz miktarı giriniz :"))
     bakiye+=ypara
@@ -12,10 +13,12 @@ def paraCekmek(bakiye):
 def krediCekmek():
     a=int(input("Lütfen çekmek istediğiniz miktarı giriniz :"))
     print("Başarıyla kredi çektiniz")
-islem=int(input("Lütfen yapmak istediğiniz işlemi seçiniz \n1-Para Yatırmak\n2-Para Çekmek\n3-Kredi Çekmek\n"))
-if islem==1:
-    paraYatirmak(bakiye)
-elif islem==2:
-    paraCekmek(bakiye)
-elif islem==3:
-    krediCekmek()
+
+while (islem!="Q"):
+    islem = str(input("Lütfen yapmak istediğiniz işlemi seçiniz \n1-Para Yatırmak\n2-Para Çekmek\n3-Kredi Çekmek\n"))
+    if islem == "1":
+        paraYatirmak(bakiye)
+    elif islem == "2":
+        paraCekmek(bakiye)
+    elif islem == "3":
+        krediCekmek()
